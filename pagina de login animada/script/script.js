e59f423f7ef5557fd8f1e6438d6107c81675bf58
Dev Armando Rocha <mandoqxo@gmail.com>
2024-08-02 02:53:56 -0300

    Update script.js

--- pagina de login animada/script/script.js
+++ pagina de login animada/script/script.js
@@ -1,24 +1,13 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     // Obtendo referências aos elementos
     const container = document.getElementById("container");
-    const registerBtn = document.getElementById('criar-conta');
-    const loginBtn = document.querySelector('.form-container.login button');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login'); // Corrigido: buscando o botão de login correto
 
-    // Verifica se os elementos foram obtidos corretamente
-    if (!container || !registerBtn || !loginBtn) {
-        console.error('Erro ao obter os elementos necessários.');
-        return;
-    }
-
-    // Adiciona classe 'active' ao container quando o botão de criar conta é clicado
-    registerBtn.addEventListener('click', () => {
-        container.classList.add('active');
-    });
-
-    // Remove classe 'active' do container quando o botão de login é clicado
-    loginBtn.addEventListener('click', () => {
-        container.classList.remove('active');
-    });
+    registerBtn.addEventListener(`click`, () => {
+        container.classList.add(`active`);
+    })
+    loginBtn.addEventListener(`click`, () => {
+        container.classList.remove(`active`);
+    })
 });
-
